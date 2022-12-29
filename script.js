01fb62f9cac826cdf13a6fdbea237b0d87630c32
Tachyon <96754559+Tachycephalosaurii@users.noreply.github.com>
2022-12-28 23:36:53 -0800

    Added Background Switcher

--- script.js
+++ script.js
@@ -20,16 +20,33 @@ function goto() {
   goto.apply(this, args);
 }
 
+
+function changeBckg() {
+  let bckgs = ['focaccia', 'spices', 'risotto'];
+  let i = Math.floor(Math.random()*bckgs.length);
+  let img = $('#main-img');
+  while (img.prop('src').includes(bckgs[i])) {
+    i = Math.floor(Math.random()*bckgs.length);
+  }
+  img.animate({ opacity: 0, duration: 100 }, {
+    done: ()=>{
+      img.prop('src', `images/backgrounds/${bckgs[i]}.jpg`);
+      img.animate({ opacity: 1, duration: 100 });
+    }
+  })
+}
+
+setInterval(changeBckg, 10000); //change every ten seconds
+
 /* Header Transparency */
 setTimeout(() => goto(localStorage.getItem('scrollTo')));
 if (window.location.href.includes('index'))
   $(window).scroll(function () {
     let h = $('#bckg-nav');
-    if (window.scrollY === 0)
+    if (window.scrollY === 0) {
       h.animate({ opacity: 0 });
-    else if (Math.ceil(h.css('opacity') * 100) === 0)
+      changeBckg();
+    } else if (Math.ceil(h.css('opacity') * 100) === 0)
       h.animate({ opacity: 1 });
   });
 else $('#bckg-nav').css('opacity', 1);
-
-
