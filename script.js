96653431943073c809825920e38ce8ce799e4476
Tachyon <96754559+Tachycephalosaurii@users.noreply.github.com>
2022-12-28 21:56:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+/* Load Templates */
+$(function () {
+  $('header').load('templates/header.html');
+  $('footer').load('templates/footer.html');
+});
+
+/* Scroll and Change Pages */
+function goto() {
+  let args = Array.from(arguments), url = args.shift();
+  if (!url) return;
+  if (url.includes('.html') && !window.location.href.includes(url))
+    window.location.href = url;
+  else if (url.includes('#')) {
+    localStorage.setItem('scrollTo', url);
+    let el = $(url)[0];
+    const y = el.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({ top: y, behavior: "smooth" });
+    setTimeout(() => localStorage.removeItem('scrollTo'), 1000);
+  }
+  goto.apply(this, args);
+}
+
+/* Header Transparency */
+setTimeout(() => goto(localStorage.getItem('scrollTo')));
+if (window.location.href.includes('index'))
+  $(window).scroll(function () {
+    let h = $('#bckg-nav');
+    if (window.scrollY === 0)
+      h.animate({ opacity: 0 });
+    else if (Math.ceil(h.css('opacity') * 100) === 0)
+      h.animate({ opacity: 1 });
+  });
+else $('#bckg-nav').css('opacity', 1);
+
+
